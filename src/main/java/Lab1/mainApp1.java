07b5435de19aa343fa74a45ae5b1ffb3314f359e
josephau0226 <118112999+josephau0226@users.noreply.github.com>
2024-09-10 14:14:20 +0800

    Add lab location

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -14,6 +14,7 @@ public class mainApp1 {
         System.out.println(n+"! = " + myLibrary.factorial(n));
         System.out.println("University name: The Hong Kong University of Science and Technology");
         System.out.println("Course name(s): COMP3111/COMP3111H");
+        System.out.println("Lab location: Room 4621");
         System.out.println("Program ..Ended …");
     }
 }
