eec230b43de157c3ca81edc664143dbcb9e14f27
josephau0226 <118112999+josephau0226@users.noreply.github.com>
2024-09-10 14:11:03 +0800

    Add course name

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -13,6 +13,7 @@ public class mainApp1 {
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
         System.out.println("University name: The Hong Kong University of Science and Technology");
+        System.out.println("Course name(s): COMP3111/COMP3111H");
         System.out.println("Program ..Ended …");
     }
 }
